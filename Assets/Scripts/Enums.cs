682dd69827a20e8b9d425c331dbe30a3a1fbb2ab
Farhad Nowzari <fdn.sharp@gmail.com>
2020-05-12 13:30:31 +0200

    Initialized movement script

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,4 @@
+public enum Direction2D {
+    Left = -1,
+    Right = 1
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    #region Properties
+    public Direction2D Direction;
+    [Range(0, 100)]
+    public float Speed;
+    #endregion
+    private Rigidbody2D mainRigidBody;  
+    void Awake()
+    {
+        mainRigidBody = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
