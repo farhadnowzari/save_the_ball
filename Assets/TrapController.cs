1820094522b991eefeacee7dc2061930351e822a
Farhad Nowzari <fdn.sharp@gmail.com>
2020-05-15 20:17:52 +0200

    Added moving saws as a trap

--- Assets/TrapController.cs
+++ Assets/TrapController.cs
@@ -1,7 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class TrapController : MonoBehaviour
 {
+    public List<GameObject> Saws;
+    public float SawSpeed;
+    public float MinimumHorizontalDistance;
+    public float MaximumHorizontalDistance;
+
+    private Dictionary<int, Direction2D> directions;
+    void Awake()
+    {
+        directions = new Dictionary<int, Direction2D>();
+        for(var i = 0; i < Saws.Count; i++) {
+            if(Saws[i].transform.position.x < 0) {
+                directions[i] = Direction2D.Right;
+            } else {
+                directions[i] = Direction2D.Left;
+            }
+        }
+    }
+
+    void Update()
+    {
+        for(var i = 0; i < Saws.Count; i++) {
+            if(directions[i] == Direction2D.Right) {
+                var rigidbody = Saws[i].GetComponent<Rigidbody2D>();
+                rigidbody.velocity = Vector2.right * SawSpeed;
+            } else {
+                var rigidbody = Saws[i].GetComponent<Rigidbody2D>();
+                rigidbody.velocity = Vector2.right * -SawSpeed;
+            }
+            if(Saws[i].transform.position.x > MaximumHorizontalDistance) {
+                directions[i] = Direction2D.Left;
+            } else if(Saws[i].transform.position.x < MinimumHorizontalDistance) {
+                directions[i] = Direction2D.Right;
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         var targetObject = other.gameObject;
