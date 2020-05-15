8da4e8b5c8dc264e7afb5f512aaae29e46e53fbf
Farhad Nowzari <fdn.sharp@gmail.com>
2020-05-15 21:33:40 +0200

    A character added to the game

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,13 +5,18 @@ public class MovementController : MonoBehaviour
 {
     #region Properties
     public Direction2D Direction;
+    private Direction2D direction;
     [Range(0, 100)]
     public float SpeedX;
     #endregion
-    private Rigidbody2D mainRigidBody;  
+    private Rigidbody2D mainRigidBody;
+
+    private Animator animator;
     void Awake()
     {
         mainRigidBody = GetComponent<Rigidbody2D>();   
+        animator = GetComponent<Animator>();
+        direction = Direction;
     }
 
     void Update()
@@ -22,10 +27,16 @@ public class MovementController : MonoBehaviour
     void FixedUpdate()
     {
         move();
+        runAnimations();
     }
 
     void setDirection() {
+        if(!InputUtils.MovingHorizontaly()) return;
         this.Direction = InputUtils.GoingRight() ? Direction2D.Right : Direction2D.Left;
+        if((Direction == Direction2D.Left || Direction == Direction2D.Right) && direction != Direction) {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, 0);
+            direction = Direction;
+        }
     }
 
     void move() {
@@ -33,5 +44,18 @@ public class MovementController : MonoBehaviour
         var velocityX = movingHorizontaly ? SpeedX * (int)Direction : 0;
         var velocityY = mainRigidBody.velocity.y;
         mainRigidBody.velocity = new Vector2(velocityX, velocityY);
+    }
+
+    void runAnimations() {
+        if(mainRigidBody.velocity.y < 0) {
+            animator.SetBool("grounded", true);
+        } else {
+            animator.SetBool("grounded", false);
+        }
+        if(InputUtils.MovingHorizontaly()) {
+            animator.SetFloat("speed", 1);
+        } else {
+            animator.SetFloat("speed", 0);
+        }
     }
 }
