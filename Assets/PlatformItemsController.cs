d425ccadc1bc21d5a87d9ddcd1b394fc72cc5723
Farhad Nowzari <fdn.sharp@gmail.com>
2020-05-16 11:31:09 +0200

    Item spawning added

---
+++ Assets/PlatformItemsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformItemsController : MonoBehaviour
+{
+    public GameObject Coin;
+    public GameObject Diamond;
+    public List<Transform> Placeholders;
+    [Range(0, 100)]
+    public float ProbabilityToSpawnAnItem;
+    [Range(0, 100)]
+    public float ProbabilityToSpawnDiamond;
+    public bool SpawnItems = true;
+
+    void Awake()
+    {
+        if(!SpawnItems) return;
+        foreach(var placeholder in Placeholders) {
+            if(!canSpawnItem()) continue;
+            if(canSpawnDiamond()) {
+                spawnItem(Diamond, placeholder);
+                continue;
+            }
+            spawnItem(Coin, placeholder);
+
+        }
+    }
+
+    bool canSpawnItem() {
+        var randomNumber = Random.Range(0, 101);
+        return randomNumber <= ProbabilityToSpawnAnItem;
+    }
+    bool canSpawnDiamond() {
+        var randomNumber = Random.Range(0, 101);
+        return randomNumber <= ProbabilityToSpawnDiamond;
+    }
+
+    void spawnItem(GameObject spawnItem, Transform placeholder) {
+        var position = new Vector3(placeholder.position.x, placeholder.position.y, 0);
+        Instantiate(spawnItem, position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/BonusItemsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusItemsController : MonoBehaviour
+{
+    public int Bonus;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var targetGameObject = other.gameObject;
+        if(targetGameObject.tag == "Player") {
+            Destroy(gameObject);
+        }
+    }
+}
