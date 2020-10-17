0f9686ab91ed99b9883a688e5557fa53ad2b55ee
farhadnowzari <fdn.sharp@gmail.com>
2020-10-17 16:27:57 +0200

    Added touch controls for mobile

--- Assets/Scripts/Utils/InputUtils.cs
+++ Assets/Scripts/Utils/InputUtils.cs
@@ -6,16 +6,31 @@ namespace Utils.InputUtils {
     public static class InputUtils {
         public static bool GoingRight() {
             var horizontalAxis = Input.GetAxisRaw(InputAxes.Horizontal.ToString());
-            return horizontalAxis > 0;
+            return horizontalAxis > 0 || TouchingRight();
         }
 
         public static bool GoingLeft() {
             var horizontalAxis = Input.GetAxisRaw(InputAxes.Horizontal.ToString());
-            return horizontalAxis < 0;
+            return horizontalAxis < 0 || TouchingLeft();
         }
 
         public static bool MovingHorizontaly() {
             return InputUtils.GoingRight() || InputUtils.GoingLeft();
+        }
+
+        public static bool TouchingLeft() {
+            if(!Touching()) return false;
+            var touch = Input.GetTouch(0);
+            return touch.position.x < Screen.width / 2;
+        }
+        public static bool TouchingRight() {
+            if(!Touching()) return false;
+            var touch = Input.GetTouch(0);
+            return touch.position.x > Screen.width / 2;
+        }
+
+        public static bool Touching() {
+            return Input.touchCount > 0;
         }
     }
 }
